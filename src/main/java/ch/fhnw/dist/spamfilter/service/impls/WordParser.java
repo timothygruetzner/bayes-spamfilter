4f0cd92a6e2b11ead39bb3c570a19a41ca4485ba
Timothy Gruetzner <timothy.gruetzner@students.fhnw.ch>
2021-10-18 12:55:53 +0200

    feat: Add file reading infrastructure

--- src/main/java/ch/fhnw/dist/spamfilter/service/impls/WordParser.java
+++ src/main/java/ch/fhnw/dist/spamfilter/service/impls/WordParser.java
@@ -2,11 +2,14 @@ package ch.fhnw.dist.spamfilter.service.impls;
 
 import ch.fhnw.dist.spamfilter.service.Parser;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 public class WordParser implements Parser {
     @Override
-    public String[] getWords(InputStream fileStream) {
-        return new String[0];
+    public String[] getWords(InputStream fileStream) throws IOException {
+        return new String(fileStream.readAllBytes(), StandardCharsets.UTF_8)
+                .split(" ");
     }
 }
