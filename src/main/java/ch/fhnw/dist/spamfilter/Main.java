db55b18ea322c748477609b0b744a17d8feda8b9
Timothy Gruetzner <timothy.gruetzner@students.fhnw.ch>
2021-10-18 13:23:54 +0200

    feat: Add program flow

--- src/main/java/ch/fhnw/dist/spamfilter/Main.java
+++ src/main/java/ch/fhnw/dist/spamfilter/Main.java
@@ -2,6 +2,7 @@ package ch.fhnw.dist.spamfilter;
 
 import ch.fhnw.dist.spamfilter.service.NaiveBayes;
 import ch.fhnw.dist.spamfilter.service.Parser;
+import ch.fhnw.dist.spamfilter.service.Prediction;
 import ch.fhnw.dist.spamfilter.service.impls.NaiveBayesImpl;
 import ch.fhnw.dist.spamfilter.service.impls.WordParser;
 
@@ -18,6 +19,8 @@ public class Main {
     public static final String HAM_CALIBRATE_SET = "ham-kalibrierung.zip";
     public static final String SPAM_TRAINING_SET = "spam-anlern.zip";
     public static final String SPAM_CALIBRATE_SET = "spam-kalibrierung.zip";
+    public static final String HAM_TEST_SET = "ham-test.zip";
+    public static final String SPAM_TEST_SET = "spam-test.zip";
 
     private final Parser wordParser;
     private final NaiveBayes bayesImpl;
@@ -28,28 +31,56 @@ public class Main {
     }
 
     public void run() throws IOException {
-        this.bayesImpl.train(getZipContents(SPAM_TRAINING_SET).toArray(new String[0][0]), getZipContents(HAM_TRAINING_SET).toArray(new String[0][0]));
+        System.out.println("*****************************************************************");
+        System.out.println("*            Naive Bayes Spam Filter                            *");
+        System.out.println("*            DIST HS 2021                                       *");
+        System.out.println("*            Impl. by Nathanael Weber & Timothy Grützner        *");
+        System.out.println("*****************************************************************");
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Using Configuration:");
+        System.out.printf("Alpha:                         %f%n", NaiveBayesImpl.α);
+        System.out.printf("Spam Threshold (|P(H) - P(S)|: %f%n", NaiveBayesImpl.THRESHOLD);
+        System.out.println();
+
+        String[][] spamTrainingSet = getZipContents(SPAM_TRAINING_SET).toArray(new String[0][0]);
+        String[][] hamTrainingSet = getZipContents(HAM_TRAINING_SET).toArray(new String[0][0]);
+
+        this.bayesImpl.train(spamTrainingSet, hamTrainingSet);
+        System.out.printf("Trained with %d spam emails and %d ham emails%n%n", spamTrainingSet.length, hamTrainingSet.length);
+
+        List<String[]> hamTestfiles = getZipContents(HAM_TEST_SET);
+        System.out.printf("Testing with %d HAM files...", hamTestfiles.size());
+        long correctHamGuesses = hamTestfiles.stream()
+                .map(this.bayesImpl::predict)
+                .filter(p -> p.getType() == Prediction.PredictionType.HAM)
+                .count();
+        System.out.printf("Guessed %d / %d HAM correctly (%f%%)", correctHamGuesses, hamTestfiles.size(), (double)correctHamGuesses/hamTestfiles.size());
+
+        List<String[]> spamTestfiles = getZipContents(HAM_TEST_SET);
+        long correctSpamGuesses = hamTestfiles.stream()
+                .map(this.bayesImpl::predict)
+                .filter(p -> p.getType() == Prediction.PredictionType.HAM)
+                .count();
+        System.out.printf("Guessed %d / %d SPAM correctly (%f%%)", correctSpamGuesses, spamTestfiles.size(), (double)correctSpamGuesses/spamTestfiles.size());
+
+
     }
 
     private List<String[]> getZipContents(String fileName) throws IOException {
-        ZipFile zipFile = null;
-        try {
-            zipFile = new ZipFile(getClass().getClassLoader().getResource(fileName).getFile());
+        try (ZipFile zipFile = new ZipFile(getClass().getClassLoader().getResource(fileName).getFile())) {
             Enumeration<? extends ZipEntry> zipEntries = zipFile.entries();
 
             List<String[]> content = new ArrayList<>();
             while (zipEntries.hasMoreElements()) {
                 ZipEntry zipEntry = zipEntries.nextElement();
-                try (InputStream stream = zipFile.getInputStream(zipEntry)){
+                try (InputStream stream = zipFile.getInputStream(zipEntry)) {
                     content.add(wordParser.getWords(stream));
                 }
             }
 
             return content;
-        } finally {
-            if(zipFile != null) {
-                zipFile.close();
-            }
         }
     }
 
