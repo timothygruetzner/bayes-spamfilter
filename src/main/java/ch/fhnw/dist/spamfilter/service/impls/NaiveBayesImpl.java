a97cdeed76fa6fd130b173350152412e05e7c785
Nathanael Weber <nathanael@ibw-informatik.ch>
2021-10-19 13:53:40 +0200

    fix: use every word which either is in ham or spam

--- src/main/java/ch/fhnw/dist/spamfilter/service/impls/NaiveBayesImpl.java
+++ src/main/java/ch/fhnw/dist/spamfilter/service/impls/NaiveBayesImpl.java
@@ -7,7 +7,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class NaiveBayesImpl implements NaiveBayes {
-    public final static double ALPHA = 1;
+    public final static double ALPHA = 0.00000000001;
     public final static double THRESHOLD = 1;
     public final static double BIAS = 0.5;
 
@@ -70,7 +70,7 @@ public class NaiveBayesImpl implements NaiveBayes {
 
     private double calculateProbability(String[] words) {
         Double summed = Arrays.stream(words)
-                .filter(word -> spamWordCountPerMail.containsKey(word) && hamWordCountPerMail.containsKey(word))
+                .filter(word -> spamWordCountPerMail.containsKey(word) || hamWordCountPerMail.containsKey(word))
                 .map(word -> {
                     double nSpam = spamWordCountPerMail.getOrDefault(word, ALPHA);
                     double nHam = hamWordCountPerMail.getOrDefault(word, ALPHA);
